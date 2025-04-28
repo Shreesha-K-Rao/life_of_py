5cbd97cf1848bae5b6ba5a885412ce38f4c17c90
Shreesha-K-Rao <73553707+Shreesha-K-Rao@users.noreply.github.com>
2025-04-28 13:34:37 +0530

    First Print Statement with God's Blessings

---
+++ Week_1/Day_One.py
@@ -0,0 +1,7 @@
+
+print("""Devare,Nimage Hrutpurvaka Namaskaragalu! 
+Indininda Nanu Python Programing Language Kalibeku anta Dhrudavaada Manassannu Madiddene. 
+Dayavittu Nimma Ashirvaada&Krupe nanna meleirali 
+yendu Hrudpurvaka Prarthane!!!
+inthi nimma preethiya shishya, 
+            Shreesha Krishna Rao""")
