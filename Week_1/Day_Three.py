4a97dacb96ebc38d795a8c06188fb3d7b83e9411
Shreesha-K-Rao <73553707+Shreesha-K-Rao@users.noreply.github.com>
2025-05-01 23:58:17 +0530

    Day 4 Beginning

--- Week_1/Day_Three.py
+++ Week_1/Day_Three.py
@@ -6,6 +6,13 @@
 # for i in range(3,9,11):
 #   print(i)
 
-for i in range(1,5):
-    print(f"{i}^2 = {i*i}")
+# for i in range(1,5):
+#     print(f"{i}^2 = {i*i}")
 
+# for i in range (1,11):
+#     print(f"{i}X7 = {i*7}")
+
+for i in range(5):
+    for j in range(5):
+        print("X",end="")
+print("Y",end="")
